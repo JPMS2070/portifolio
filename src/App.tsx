e0afc2dedd8b4140f919915f30ff3cacd1ed0f91
Joao Pedro <joaopedromartinsdosantos360@gmail.com>
2026-08-04 20:44:31 -0300

    primeiro commit do portfólio

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import './App.css'
+
+
+
+function App() {
+
+  return (
+      <>
+      <main className='grid grid-cols-2 gap-4'>
+        <article>
+          <img src="img\profile.png" alt="" height="20px"/>
+          <p>Desenvolvedor RPA/FullStack</p>
+          Trabalho atualmente com Python, FastAPI, React, JavaScript, HTML5 e CSS3, construindo soluções que unem backend, automação e frontend. Tenho paixão por transformar ideias em projetos reais e úteis — uso a tecnologia para agilizar e automatizar processos que, no dia a dia, tomariam muito mais tempo manual. Sou movido por curiosidade: gosto de aprender rápido, testar na prática e acompanhar cada projeto até o resultado final, vendo a ideia ganhar forma.
+        </article>
+        <section>
+          <div><h2>Habilidades tecnologias</h2></div>
+          <ul>
+          <img src="img/react.png" alt="" />
+          <img src="img/python.png" alt="" />
+          <img src="img/html.png" alt="" />
+          <img src="img/css-3.png" alt="" />
+          <img src="img/js.png" alt="" />
+          <img src="img/post.png" alt=""/>
+          </ul>
+        </section>
+      </main>
+      </>
+  )
+}
+
+export default App
